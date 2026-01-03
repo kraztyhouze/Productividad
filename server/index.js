03dfba3d049a16e21da95d94d1e143a635521c84
Juanma <tu_email@ejemplo.com>
2026-01-03 15:24:57 +0100

    Use Regex for catch-all route to fully fix Express 5 issue

--- server/index.js
+++ server/index.js
@@ -201,7 +201,7 @@ app.get('/health', (req, res) => {
 // --- Serve Frontend (Production/Docker) ---
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('/*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
